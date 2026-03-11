b857fa7f8b5c6f34c26a327cb9a423c20b213b3f
6JAPA7 <enzoboquim2019@gmail.com>
2026-03-11 17:09:48 -0300

    Refactor add_produto function and input creation

--- catalogodeprodutos.py
+++ catalogodeprodutos.py
@@ -2,45 +2,73 @@ import tkinter as tk
 from tkinter import ttk
 import mysql.connector
 
-def save():
-    lista.append(input.get())
+def criar_input(telae, nome):
+        linha = tk.Frame(telae)
+        linha.pack(pady=5)
+        label = tk.Label(linha, text=nome, fg="black")
+        label.pack(side="left")
+        input = nome = tk.Entry(linha)
+        input.pack(side="left")
+        return input
+
+
 
 
 def add_produto():
+    def save():
+        lista = [id_atual]
+        for caixa in caixas:
+            lista.append(caixa.get())
+        print(f"lista: {lista}")
+        
+        itens = tuple(lista)
+        colunassema = ", ".join(colunas)
+        
+        
+        cmd = f"INSERT INTO new_table ({colunassema}) VALUES {itens};"
+        print(cmd)
+        print(f"\033[1;31;43mitens: {itens}\033[m")
+        db = mysql.connector.connect(host="localhost", user="root", password="Japaa2222", database="new_schema")
+        cursor = db.cursor()
+
+        
+        cursor.execute(cmd)
+        db.commit()
+        cursor.close()
+        db.close()
+        tela_add.destroy()
+        
+        #atualizar tabela e fechar janela
     
     tela_add = tk.Toplevel()
     tela.title('adicionar produto')
+    id_atual = linhas+1
+    caixas = []
+    for c in colunas[1:]:
+        caixa = criar_input(tela_add, c)
+        caixas.append(caixa)
+
+    salvar = tk.Button(tela_add, text="confirmar", command=save)
+    salvar.pack(side="left")
+    cancelar = tk.Button(tela_add, text="cancelar")
+    cancelar.pack(side="left")
+        
 
     
 
     #fazer fução que cria input ja com label do lado, e coloca os inputs numa lista ou dicionario, depois pega os dados de todos inputs das lista
     
-    class input:
-        def __init__(self, nome:str, tela):
-            self.nome = nome
-            self.tela = tela
-        def criar_input(self):
-            linha = tk.Frame(self.tela)
-            linha.pack(pady=5)
-            label = tk.Label(linha, text=self.nome, fg="black")
-            label.pack(side="left")
-            input = self.nome = tk.Entry(linha)
-            input.pack(side="left")
-            
-            
-            
-    lista = []
-    for c in colunas[1:]:
-        
-    produto = input("produto", tela_add)
-    produto.criar_input()
-    preco = input("preço", tela_add)
-    preco.criar_input()
+
     
-    salvar = tk.Button(tela_add, text="confirmar", command=save)
-    salvar.pack(side="left")
-    cancelar = tk.Button(tela_add, text="cancelar")
-    cancelar.pack(side="left")
+            
+            
+            
+    
+    
+        
+        
+    
+    
     
 
    #produto(nome, preco, linhas)
@@ -48,6 +76,7 @@ def add_produto():
 
 
 def atualizar_tabela():
+    
     table = ttk.Treeview(tela)
     
     table['columns'] = colunas
@@ -67,7 +96,7 @@ def atualizar_tabela():
 
     table.pack(side="left")
 
-    #cursor.execute("INSERT INTO new_table (id, nome, descricao)VALUES(id, nome, preco)")
+    
 
 #pegando dados da tabela mysql
 db = mysql.connector.connect(host="localhost", user="root", password="Japaa2222", database="new_schema")
